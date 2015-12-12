e9a8a67337a0d2ad8e5aab27755b2c7e5518216c
sprawls <alex3521@hotmail.fr>
2015-12-11 21:07:19 -0500

    Basic unity store assets

---
+++ GeneticAlgorithm/Assets/PrimitivePlus/Documentation/ReadMe.txt
@@ -0,0 +1,31 @@
+﻿////////////////////////////////////////////////////////////
+// Primitive Plus adds over 40+ primitive shapes to Unity //
+////////////////////////////////////////////////////////////
+//
+///////////
+// VIDEO //
+///////////
+//
+// How To Use: https://youtu.be/1EIifPNSSOk
+//
+///////////
+// USAGE //
+///////////
+//
+// - In your Hierarchy select Create > Primitive Plus > [Shape]
+// - Your new shape will be placed on the scene view's pivot
+// - UNITY_5: Right clicking a GameObject will add it as a child GameObject at Vector.zero
+// - New shapes use a basic Mesh Collider by default
+// - Primitive Plus Material allows you to share or use new materials on each shape
+// - - > No need to create a new material, just open up the attached material and change the colour / texture
+// - > New Material - makes a new Material default-diffuse or standard(unity5) on the MeshRenderer
+// - > Share Material - shares the same material from the first selected game object
+//
+/////////////
+// CONTACT //
+/////////////
+//
+// Twitter: @ZeroLogics
+// Site: 	mikedesjardins.ca
+// e-mail: 	mike.desjardins@outlook.com
+//
---
+++ GeneticAlgorithm/Assets/PrimitivePlus/Editor/PrimitivePlusMaterialEditor.cs
@@ -0,0 +1,52 @@
+﻿using UnityEditor;
+using UnityEngine;
+using PrimitivePlus;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PrimitivePlusEditor
+{
+	[CustomEditor(typeof(PrimitivePlusMaterial)), CanEditMultipleObjects]
+	public class PrimitivePlusMaterialEditor : Editor
+	{
+
+		private void OnDisable()
+		{
+			Resources.UnloadUnusedAssets();
+		}
+
+		public override void OnInspectorGUI ()
+		{
+			GameObject[] gameObjects = Selection.gameObjects;
+			List<PrimitivePlusMaterial> materials = new List<PrimitivePlusMaterial>();
+			Material material = gameObjects[0].GetComponent<MeshRenderer>().sharedMaterial;
+
+			for(int i = 0; i < gameObjects.Length; i++)
+			{
+				if(gameObjects[i].GetComponent<PrimitivePlusMaterial>() != null)
+					materials.Add(gameObjects[i].GetComponent<PrimitivePlusMaterial>());
+			}
+
+			GUILayout.Space(10);
+			GUILayout.BeginHorizontal();
+			GUILayout.Space(10);
+			if(materials.Count > 1 && GUILayout.Button("Share Materials", EditorStyles.miniButtonLeft))
+			{
+				for(int i = 0; i < materials.Count; i++)
+				{
+					materials[i].SetSharedMaterial(material);
+				}
+			}
+			GUIStyle buttonStyle = (materials.Count > 1) ? EditorStyles.miniButtonRight : EditorStyles.miniButton;
+			if(GUILayout.Button("New Material", buttonStyle))
+			{
+				for(int i = 0; i < materials.Count; i++)
+				{
+					materials[i].SetNewMaterial();
+				}
+			}
+			GUILayout.Space(15);
+			GUILayout.EndHorizontal();
+		}
+	}
+}
