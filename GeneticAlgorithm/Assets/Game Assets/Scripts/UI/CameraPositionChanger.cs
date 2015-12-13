b4260e44f8fa03e33761bfde4e7c764e0579b432
Antoine Brouillette <antoineqc@hotmail.com>
2015-12-12 19:27:25 -0500

    Working on menu, training. Completed menu & training base art

---
+++ GeneticAlgorithm/Assets/Game Assets/Scripts/UI/CameraPositionChanger.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPositionChanger : MonoBehaviour {
+
+    [Header("Menu")]
+    [SerializeField]
+    private Vector3 menuPosition;
+
+    [SerializeField]
+    private float menuSize;
+
+    [Header("Training")]
+    [SerializeField]
+    private Vector3 trainingPosition;
+
+    [SerializeField]
+    private float trainingSize;
+
+    [Header("Base value")]
+    [SerializeField]
+    private bool menuActive;
+
+    public void SwitchPosition(float timeTaken) {
+        StopAllCoroutines();
+        StartCoroutine(SwitchAnim(menuActive ? trainingPosition : menuPosition, menuActive ? trainingSize : menuSize, timeTaken));
+    }
+
+    IEnumerator SwitchAnim(Vector3 pos, float size, float timeTaken) {
+        float time = 0;
+        Camera camera = GetComponent<Camera>();
+        Vector3 startingPos = transform.position;
+        float startingSize = camera.orthographicSize;
+
+        //Lerp to target
+        while (pos != transform.position) {
+            time += Time.deltaTime;
+            transform.position = Vector3.Lerp(startingPos, pos, time / timeTaken);
+            camera.orthographicSize = Mathf.Lerp(startingSize, size, time / timeTaken);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
---
+++ GeneticAlgorithm/Assets/Game Assets/Scripts/UI/UI_Switcher.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI_Switcher : MonoBehaviour {
+
+    [Header("Component")]
+    [SerializeField]
+    private CameraPositionChanger camera;
+
+    [SerializeField]
+    private CanvasGroup menuUI;
+
+    [SerializeField]
+    private CanvasGroup trainingUI;
+
+    public void Switch(bool toTraining) {
+        camera.SwitchPosition(1.5f);
+        StartCoroutine(toTraining ? OpenTrainingUI() : OpenMenuUI());
+    }
+
+    IEnumerator OpenMenuUI() {
+        float time = 0;
+
+        trainingUI.interactable = false;
+        trainingUI.blocksRaycasts = false;
+        while (menuUI.alpha != 1) {
+            time += Time.deltaTime;
+            if (time <= 1.5f) trainingUI.alpha = Mathf.Lerp(1, 0, time);
+            else menuUI.alpha = Mathf.Lerp(0, 1, (time - 0.5f) / 2);
+            yield return new WaitForEndOfFrame();
+        }
+        menuUI.interactable = true;
+        menuUI.blocksRaycasts = true;
+    }
+
+    IEnumerator OpenTrainingUI() {
+        float time = 0;
+
+        menuUI.interactable = false;
+        menuUI.blocksRaycasts = false;
+        while (trainingUI.alpha != 1) {
+            time += Time.deltaTime;
+            if (time <= 1.5f) menuUI.alpha = Mathf.Lerp(1, 0, time); 
+            else trainingUI.alpha = Mathf.Lerp(0, 1, (time - 0.5f) / 2);
+            yield return new WaitForEndOfFrame();
+        }
+        trainingUI.interactable = true;
+        trainingUI.blocksRaycasts = true;
+    }
+}
